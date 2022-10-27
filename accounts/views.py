674e0db28ea3689dcd81da1fcef130bab81067fb
lsb0709 <dltkdqor0709@gmail.com>
2022-10-27 14:16:40 +0900

    accounts/user

--- accounts/views.py
+++ accounts/views.py
@@ -26,3 +26,11 @@ def signup(request):
         "form":form,
     }
     return render(request, "accounts/signup.html", context)
+
+def detail(request, user_pk):
+    user = get_user_model().objects.get(pk=user_pk)
+
+    context = {
+        "user": user,
+    }
+    return render(request, "account/detail.html", context)
