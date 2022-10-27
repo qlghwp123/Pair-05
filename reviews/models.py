9a067fa835772f9d284b8f4c517ec7e7a47d1849
suyoung049 <suyoung049@gmail.com>
2022-10-27 15:18:10 +0900

    ADD:create,delete,update

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,40 @@
+from ssl import create_default_context
 from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+from django.conf import settings
 
-# Create your models here.
+
+class Review(models.Model):
+    title = models.CharField(max_length=80)
+    content = models.TextField()
+    movie_name = models.CharField(max_length=50)
+    RATING = [
+        (1, "★"),
+        (2, "★★"),
+        (3, "★★★"),
+        (4, "★★★★"),
+        (5, "★★★★★"),
+    ]
+
+    grade = models.IntegerField(choices=RATING, default=None)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = ProcessedImageField(
+        upload_to="images/",
+        blank=True,
+        processors=[ResizeToFill(1200, 960)],
+        format="JPEG",
+        options={"quality": 80},
+    )
+    user = models.Foreignkey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="like_reviews"
+    )
+
+
+# class comment(models.Model):
+#     content = models.TextField(max_length=300, blank=False)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     review = models.Foreignkey(Review, on_delet=models.CASCADE)
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
