69051a77184d32aa47bdea5d169b9ecbc99b5341
lsb0709 <dltkdqor0709@gmail.com>
2022-10-28 16:25:25 +0900

    account/detail

--- accounts/models.py
+++ accounts/models.py
@@ -4,3 +4,5 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
+    instagram = models.TextField(max_length=20)
+    github = models.TextField(max_length=20)
