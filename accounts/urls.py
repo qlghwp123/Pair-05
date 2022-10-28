28e2c6c583ebdc727cf51a092edc97da78db4497
lsb0709 <dltkdqor0709@gmail.com>
2022-10-28 14:08:16 +0900

    accounts/update

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     path("<int:user_pk>/", views.detail, name='detail'),
     path("login/", views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-    # path('password/', views.change_password, name='change_password'),
+    path('update/', views.update, name='update'),
+    path('password/', views.change_password, name='change_password'),
     path('<int:user_pk>/follow/', views.follow, name='follow'),
 
 ]
